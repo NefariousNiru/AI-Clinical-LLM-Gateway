40d77d4ee86677ed22a2e6da27c70338ceed6f4e
NefariousNiru <nirupomboseroy99@gmail.com>
2025-09-11 01:36:43 -0400

    refactor

--- gateway/util/functions.py
+++ gateway/util/functions.py
@@ -1,42 +1,5 @@
 # gateway/util/functions.py
 import json
-from gateway.grader.v1.grader_pb2 import RubricPayload, DrugRelatedProblem
-
-
-def serialize(rubric: RubricPayload, problems: list[DrugRelatedProblem]):
-    rubric_dict = {
-        "rubric_id": rubric.rubric_id,
-        "guideline_hint": rubric.guideline_hint,
-        "sections": [
-            {
-                "id": s.id,
-                "title": s.title,
-                "max_points": s.max_points,
-                "evaluation_question": s.evaluation_question,
-                "criteria": [
-                    {
-                        "key": c.key,
-                        "type": c.type,
-                        "verbiage": c.verbiage,
-                        "weight": c.weight if c.HasField("weight") else None,
-                    }
-                    for c in s.criteria
-                ],
-            }
-            for s in rubric.sections
-        ],
-    }
-    problems_dict = [
-        {
-            "is_priority": p.is_priority,
-            "identification": p.identification,
-            "explanation": p.explanation,
-            "plan_recommendation": p.plan_recommendation,
-            "monitoring": p.monitoring,
-        }
-        for p in problems
-    ]
-    return rubric_dict, problems_dict
 
 
 def create_prompt(
